20cde352c6efdd008299a9714cc482b41d8782c7
Caiden Kumar <valiec2019@gmail.com>
2022-03-02 08:45:39 -0500

    Major code cleanup, started on Wiktionary parser

---
+++ clean_data.py
@@ -0,0 +1,40 @@
+import re
+
+
+# cleans up extra punctuation and formatting from a word
+# will strip newlines, spaces, tabs, and hyphens, and convert word to lowercase
+def clean_word(raw_word, headwords):
+    lower_word = raw_word.lower()
+    # TODO handle diacritics
+    regex = re.compile('[^a-z]')  # remove all non-alphabetic characters
+    cleaned_word = regex.sub("", lower_word)
+
+    if cleaned_word.endswith("s") and cleaned_word[:-1] in headwords:  # potential plural
+        cleaned_word = cleaned_word[:-1]
+
+    elif cleaned_word.endswith("ing") and cleaned_word[:-3] in headwords:  # potential participle or gerund
+        cleaned_word = cleaned_word[:-3]
+
+    elif cleaned_word.endswith("ed") and cleaned_word[:-2] in headwords:  # potential past tense verb
+        cleaned_word = cleaned_word[:-2]
+    else:
+        pass
+
+    # words ending in -er, -ly, or similar are going to have their own entries
+
+    return cleaned_word
+
+
+# cleans out common and duplicate words (I could turn off deduplication if we want the duplication)
+def clean_entries(word_data_raw, exclusions, headwords):
+    word_data = {}
+    for headword in word_data_raw.keys():
+        definition_str = word_data_raw[headword]
+        definition_wordlist = definition_str.split()
+        definition_cleaned_unique = []
+        for word in definition_wordlist:
+            word_clean = clean_word(word, headwords)  # clean word first to make sure exclusions don't mess up
+            if word_clean in headwords and word_clean not in definition_cleaned_unique and word_clean not in exclusions:
+                definition_cleaned_unique.append(word_clean)  # convert words to all-lowercase
+        word_data[headword] = definition_cleaned_unique
+    return word_data
--- parseword.py
+++ parseword.py
@@ -13,185 +13,200 @@ allowed_types = ["Noun", "Adjective", "Adverb", "Conjunction", "Preposition", "I
 
 
 def find_word_in_partial_template(tmpl, startind):
-  for arg in tmpl.split("|")[startind:]:
-    if "=" not in arg:
-      return arg
+    for arg in tmpl.split("|")[startind:]:
+        if "=" not in arg and arg != "en":
+            return arg
 
-def handle_wikitemplate(wikitemplate):  # TODO fix this!!!
-  #print("template found: "+wikitemplate)
 
-  of_regex = re.compile("\{\{.* of$")
+def handle_wikitemplate(wikitemplate):  # TODO cleanup
+    # print("template found: "+wikitemplate)
+    args = wikitemplate.replace("{", "").replace("}", "").split("|")
+    # print("args: "+str(args))
 
-  if wikitemplate.startswith("{{plural of|en|"):
-    return "$$PLURAL$$"+find_word_in_partial_template(wikitemplate, 2)
-  elif wikitemplate.startswith("{{plural of|"):
-    return "$$PLURAL$$"+find_word_in_partial_template(wikitemplate, 1)
-  elif wikitemplate.startswith("{{synonym of|"):
-    return "$$SYNONYM$$"+find_word_in_partial_template(wikitemplate, 1)
-  elif wikitemplate.startswith("{{alternative spelling of|en") or wikitemplate.startswith("{{alternative form of|en") or wikitemplate.startswith("{{archaic form of|en") or wikitemplate.startswith("{{nonstandard spelling of|en"):
-    return "$$ALTSPELL$$"+find_word_in_partial_template(wikitemplate, 2)
-  elif wikitemplate.startswith("{{alternative spelling of|") or wikitemplate.startswith("{{alternative form of|") or wikitemplate.startswith("{{nonstandard spelling of|") or wikitemplate.startswith("{{archaic form of|"):
-    return "$$ALTSPELL$$"+find_word_in_partial_template(wikitemplate, 1)
-  elif wikitemplate.startswith("{{n-g") or wikitemplate.startswith("{{non-gloss definition"):
-    return parse_wikilinks(wikitemplate.split("|")[1].strip().replace("}}", ""))
-  elif wikitemplate.startswith("{{l") or wikitemplate.startswith("{{link"):
-    return parse_wikilinks(wikitemplate.split("|")[1].strip().replace("}}", ""))
-  elif wikitemplate.startswith("{{alternative spelling of|en") or wikitemplate.startswith("{{alternative form of|en") or wikitemplate.startswith("{{archaic form of|en") or wikitemplate.startswith("{{nonstandard spelling of|en") or wikitemplate.startswith("{{alt form|en") or wikitemplate.startswith("{{alt case|en") or wikitemplate.startswith("{{alt sp|en"):
-    return "$$ALTSPELL$$"+find_word_in_partial_template(wikitemplate, 2)
-  elif wikitemplate.startswith("{{alternative spelling of") or wikitemplate.startswith("{{alternative form of") or wikitemplate.startswith("{{nonstandard spelling of") or wikitemplate.startswith("{{archaic form of") or wikitemplate.startswith("{{alt form") or wikitemplate.startswith("{{alt case") or wikitemplate.startswith("{{alt sp"):
-    return "$$ALTSPELL$$"+find_word_in_partial_template(wikitemplate, 1)
-  elif of_regex.match(wikitemplate.split("|")[0]):
-    if "|en|" in wikitemplate:
-      wikitemplate = wikitemplate.replace("|en|", "|")
-    return "$$ALTFORM$$"+find_word_in_partial_template(wikitemplate.replace("}}", ""), 1)
-  return ""
+    of_regex = re.compile(".* of$")
+
+    if args[0] == "plural of":
+        return "$$PLURAL$$" + find_word_in_partial_template(wikitemplate, 1)
+
+    elif args[0] == "synonym of":
+        return "$$SYNONYM$$" + find_word_in_partial_template(wikitemplate, 1)
+
+    elif args[0] == "alternative spelling of" or args[0] == "alternative form of" \
+            or args[0] == "archaic form of" or args[0] == "nonstandard spelling of" \
+            or args[0] == "alt form" or args[0] == "alt case" or args[0] == "alt sp":
+        return "$$ALTSPELL$$" + find_word_in_partial_template(wikitemplate, 1)
+
+    elif args[0] == "n-g" or args[0] == "non-gloss definition":
+        return parse_wikilinks(wikitemplate.split("|")[1].strip().replace("}}", ""))
+
+    elif wikitemplate.startswith("{{l") or wikitemplate.startswith("{{link"):
+        return parse_wikilinks(wikitemplate.split("|")[1].strip().replace("}}", ""))
+
+    elif of_regex.match(args[0]):
+        if "|en|" in wikitemplate:
+            wikitemplate = wikitemplate.replace("|en|", "|")
+        return "$$ALTFORM$$" + find_word_in_partial_template(wikitemplate.replace("}}", ""), 1)
+    return ""
+
 
 def handle_wikilink(wikilink):
-  #print("link found: "+wikilink)
-  return wikilink.replace("[", "").replace("]", "")
+    if "|" in wikilink:  # handle piped links
+        wikilink = wikilink.split("|")[1]
+    return wikilink.replace("[", "").replace("]", "")
 
 
 def parse_wikitemplates(definition):
-  processed_definition = ""
-  wikitemplate_level = 0
-  brace_state = 0
-  last_brace_type = ""
-  is_special = False
-  special_str = ""
-  wikitemplate = ""
-  for char in definition:
-    if char == "{":
-      if last_brace_type == "" or last_brace_type == "{":
-        brace_state+=1
-        last_brace_type = "{"
-      else:
-        brace_state = 0
-        last_brace_type = ""
-    if char == "}" and (last_brace_type == "" or last_brace_type == "}"):
-      if last_brace_type == "" or last_brace_type == "}":
-        brace_state+=1
-        last_brace_type = "}"
-      else:
-        brace_state = 0
-        last_brace_type = ""
-    if wikitemplate_level == 0 and char != "{":
-      processed_definition = processed_definition+char
-    if wikitemplate_level > 0:
-      wikitemplate= wikitemplate+char
-    if brace_state == 2: # either entering or exiting wikitemplate
-      if char == "{":
-        wikitemplate_level+=1
-      if char == "}":
-        wikitemplate_level-=1
-      if wikitemplate_level == 0: # exiting wikitemplate
-        result = handle_wikitemplate(wikitemplate)
-        if result.startswith("$$PLURAL") or result.startswith("$$SYNONYM") or result.startswith("$$ALTSPELL") or result.startswith("$$ALTFORM"):
-          is_special = True
-          special_str = result
-        else:
-          processed_definition = processed_definition+result
-      if wikitemplate_level == 1: # entering new wikitemplate
-        wikitemplate = "{{"
-      brace_state = 0
-      last_brace_type = ""
-  if not is_special:
-    return processed_definition
-  else:
-    return special_str
+    processed_definition = ""
+    wikitemplate_level = 0
+    brace_state = 0
+    last_brace_type = ""
+    is_special = False
+    special_str = ""
+    wikitemplate = ""
+    for char in definition:
+        if char == "{":
+            if last_brace_type == "" or last_brace_type == "{":
+                brace_state += 1
+                last_brace_type = "{"
+            else:
+                brace_state = 0
+                last_brace_type = ""
+        if char == "}" and (last_brace_type == "" or last_brace_type == "}"):
+            if last_brace_type == "" or last_brace_type == "}":
+                brace_state += 1
+                last_brace_type = "}"
+            else:
+                brace_state = 0
+                last_brace_type = ""
+        if wikitemplate_level == 0 and char != "{":
+            processed_definition = processed_definition + char
+        if wikitemplate_level > 0:
+            wikitemplate = wikitemplate + char
+        if brace_state == 2:  # either entering or exiting wikitemplate
+            if char == "{":
+                wikitemplate_level += 1
+            if char == "}":
+                wikitemplate_level -= 1
+            if wikitemplate_level == 0:  # exiting wikitemplate
+                result = handle_wikitemplate(wikitemplate)
+                if result.startswith("$$PLURAL") or result.startswith("$$SYNONYM") or result.startswith(
+                        "$$ALTSPELL") or result.startswith("$$ALTFORM"):
+                    is_special = True
+                    special_str = result
+                else:
+                    processed_definition = processed_definition + result
+            if wikitemplate_level == 1:  # entering new wikitemplate
+                wikitemplate = "{{"
+            brace_state = 0
+            last_brace_type = ""
+    if not is_special:
+        return processed_definition
+    else:
+        return special_str
+
 
 def parse_wikilinks(definition):
-  processed_definition = ""
-  in_wikilink = False
-  brace_state = 0
-  wikilink = ""
-  for char in definition:
-    if char == "[" and not in_wikilink:
-      brace_state+=1
-    if char == "]" and in_wikilink:
-      brace_state+=1
-    if not in_wikilink and char != "[":
-      processed_definition = processed_definition+char
-    if in_wikilink:
-      wikilink = wikilink+char
-    if brace_state == 2: # either entering or exiting wikilink
-      in_wikilink = not in_wikilink
-      if not in_wikilink: # exiting wikilink
-        processed_definition = processed_definition+handle_wikilink(wikilink)
-      if in_wikilink: # entering new wikilink
-        wikilink = "[["
-      brace_state = 0
-  return processed_definition
+    processed_definition = ""
+    in_wikilink = False
+    brace_state = 0
+    wikilink = ""
+    for char in definition:
+        if char == "[" and not in_wikilink:
+            brace_state += 1
+        if char == "]" and in_wikilink:
+            brace_state += 1
+        if not in_wikilink and char != "[":
+            processed_definition = processed_definition + char
+        if in_wikilink:
+            wikilink = wikilink + char
+        if brace_state == 2:  # either entering or exiting wikilink
+            in_wikilink = not in_wikilink
+            if not in_wikilink:  # exiting wikilink
+                processed_definition = processed_definition + handle_wikilink(wikilink)
+            if in_wikilink:  # entering new wikilink
+                wikilink = "[["
+            brace_state = 0
+    return processed_definition
+
 
 def process_definition(headword, definition_raw):
-  definition = definition_raw.replace("\n", " ")
-  #print(headword+": "+definition)
-  definition = parse_wikilinks(definition)
-  definition = parse_wikitemplates(definition)
-  if definition.startswith("$$PLURAL"):
-    plurals_map[headword] = definition.replace("$$PLURAL$$", "")
-  elif definition.startswith("$$SYNONYM"):
-    synonyms_map[headword] = definition.replace("$$SYNONYM$$", "")
-  elif definition.startswith("$$ALTSPELL"):
-    alt_spellings_map[headword] = definition.replace("$$ALTSPELL$$", "")
-  elif definition.startswith("$$ALTFORM"):
-    forms_map[headword] = definition.replace("$$ALTFORM$$", "")
-  else:
-    words_data[headword] = definition
-    words_data_raw[headword] = definition_raw.replace("\n", " ")
-    #print(headword)
-    #print(headword+": plural of "+definition.replace("$$PLURAL$$", ""))
+    definition_split = definition_raw.split("\n")
+    definition_preproc = ""
+    for line in definition_split:
+        if not line.startswith("#*"):  # skip quotes
+            definition_preproc += line
+    definition = definition_preproc.replace("\n", " ")
+    # print(headword+": "+definition)
+    definition = parse_wikilinks(definition)
+    definition = parse_wikitemplates(definition)
+    if definition.startswith("$$PLURAL"):
+        plurals_map[headword] = definition.replace("$$PLURAL$$", "")
+    elif definition.startswith("$$SYNONYM"):
+        synonyms_map[headword] = definition.replace("$$SYNONYM$$", "")
+    elif definition.startswith("$$ALTSPELL"):
+        alt_spellings_map[headword] = definition.replace("$$ALTSPELL$$", "")
+    elif definition.startswith("$$ALTFORM"):
+        forms_map[headword] = definition.replace("$$ALTFORM$$", "")
+    else:
+        words_data[headword] = definition
+        words_data_raw[headword] = definition_preproc.replace("\n", "$$")
+        # print(headword)
+        # print(headword+": plural of "+definition.replace("$$PLURAL$$", ""))
+
 
 def parse_file(filename):
+    # data = None
 
-  data = None
+    with open(filename) as data_file:
+        data = json.load(data_file)
 
-  with open(filename) as f:
-    data = json.load(f)
+    for entry in data['text']:
+        if 'subSections' not in entry:
+            continue
+        for entry2 in entry['subSections']:
+            if entry2['title'] == "English":
+                if 'subSections' not in entry2:
+                    continue
+                for entry3 in entry2['subSections']:
+                    if entry3['title'] in allowed_types:
+                        process_definition(data['title'], entry3['text'])
 
-  for entry in data['text']:
-    if not 'subSections' in entry:
-      continue
-    for entry2 in entry['subSections']:
-      if entry2['title'] == "English":
-        if not 'subSections' in entry2:
-          continue
-        for entry3 in entry2['subSections']:
-          if entry3['title'] in allowed_types:
-              process_definition(data['title'], entry3['text'])
 
-basepath = "/Users/valiec/Desktop/word_data/English"
+base_path = "/Users/valiec/Desktop/word_data/English"
 
-dirs = os.listdir(basepath)
+dirs = os.listdir(base_path)
 
-for dir in dirs:
-  if os.path.isdir(basepath+"/"+dir):
-    if dir != "b":
-      continue
-    files = os.listdir(basepath+"/"+dir)
-    for file in files:
-      if file.endswith(".json"):
-        parse_file(basepath+"/"+dir+"/"+file)
+for words_dir in dirs:
+    if os.path.isdir(base_path + "/" + words_dir):
+        if words_dir != "b":
+            continue
+        files = os.listdir(base_path + "/" + words_dir)
+        for file in files:
+            if file.endswith(".json"):
+                parse_file(base_path + "/" + words_dir + "/" + file)
 
-with open("word_data_out.txt", "w") as f:
-  for word in words_data.keys():
-    f.write(word+"\t"+words_data[word]+"\n")
+# parse_file("159.json")
 
-with open("word_data_out_raw.txt", "w") as f:
-  for word in words_data.keys():
-    f.write(word+"\t"+words_data_raw[word]+"\n")
+with open("word_data_out.txt", "wb") as f:
+    for word in words_data.keys():
+        f.write((word + "\t" + (words_data[word]) + "\n").encode('utf8'))
 
-with open("plurals_out.txt", "w") as f:
-  for word in plurals_map.keys():
-    f.write(word+"\t"+plurals_map[word]+"\n")
+with open("word_data_out_raw.txt", "wb") as f:
+    for word in words_data.keys():
+        f.write((word + "\t" + (words_data_raw[word]) + "\n").encode('utf8'))
 
-with open("synonyms_out.txt", "w") as f:
-  for word in synonyms_map.keys():
-    f.write(word+"\t"+synonyms_map[word]+"\n")
+with open("plurals_out.txt", "wb") as f:
+    for word in plurals_map.keys():
+        f.write((word + "\t" + (plurals_map[word]) + "\n").encode('utf8'))
 
-with open("alt_spellings_out.txt", "w") as f:
-  for word in alt_spellings_map.keys():
-    f.write(word+"\t"+alt_spellings_map[word]+"\n")
+with open("synonyms_out.txt", "wb") as f:
+    for word in synonyms_map.keys():
+        f.write((word + "\t" + (synonyms_map[word]) + "\n").encode('utf8'))
 
-with open("forms_out.txt", "w") as f:
-  for word in forms_map.keys():
-    f.write(word+"\t"+forms_map[word]+"\n")
+with open("alt_spellings_out.txt", "wb") as f:
+    for word in alt_spellings_map.keys():
+        f.write((word + "\t" + (alt_spellings_map[word]) + "\n").encode('utf8'))
+
+with open("forms_out.txt", "wb") as f:
+    for word in forms_map.keys():
+        f.write((word + "\t" + (forms_map[word]) + "\n").encode('utf8'))
