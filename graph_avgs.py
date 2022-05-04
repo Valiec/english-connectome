8e44a040cda4e1d4b348b0767d4e838c3abf9c3b
Caiden Kumar <valiec2019@gmail.com>
2022-05-04 17:18:01 -0400

    Code refactoring.

--- graph_avgs.py
+++ graph_avgs.py
@@ -24,16 +24,22 @@ plt.title("Semantic vs. Etymological Distances")
 axes.set_xlabel("Average Semantic Distances (edges)")
 axes.set_ylabel("Average Etymological Distance (edges)")
 
-# plot samples
-p1 = plt.scatter(data_x[0:80], data_y[0:80], c="tab:blue", s=10)
-p2 = plt.scatter(data_x[80:160], data_y[80:160], c="tab:orange", s=10, marker="^")
-p3 = plt.scatter(data_x[160:240], data_y[160:240], c="tab:green", s=10, marker="s")
-p4 = plt.scatter(data_x[240:320], data_y[240:320], c="tab:purple", s=10, marker="+")
-p5 = plt.scatter(data_x[320:400], data_y[320:400], c="tab:red", s=10, marker="d")
-p6 = plt.scatter(data_x[400:480], data_y[400:480], c="tab:brown", s=10, marker="x")
-p7 = plt.scatter(data_x[480:560], data_y[480:560], c="tab:pink", s=10, marker="*")
 
-plt.legend([p1, p2, p3, p4, p5, p6, p7],
-           ["2500 All Etym", "1000 All Etym", "2500 No Roots", "1000 No Roots/Affixes", "1000 Only Roots",
-            "1000 No Affixes", "1000 No Roots"])  # legend
+names = ["2500 All Etym", "1000 All Etym", "2500 No Roots", "1000 No Roots/Affixes", "1000 Only Roots",
+            "1000 No Affixes", "1000 No Roots", "2500 No Roots/Affixes", "2500 No Affixes"]
+
+colors = ["tab:blue", "tab:orange", "tab:green", "tab:purple", "tab:red", "tab:brown", "tab:pink", "tab:gray", "tab:olive"]
+
+markers = [".", "^", "s", "+", "d", "x", "*", "D", "p"]
+
+ind = 0
+
+plots = []
+
+for sample in names:
+    # plot samples
+    plots.append(plt.scatter(data_x[(ind*80):(ind*80)+80], data_y[(ind*80):(ind*80)+80], c=colors[ind], s=10, marker=markers[ind]))
+    ind+=1
+
+plt.legend(plots, names)  # legend
 plt.show()  # show graph
